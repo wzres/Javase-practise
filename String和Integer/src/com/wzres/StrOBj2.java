064f9766d807b88d4697d55b6c4ed37362dfbc3d
wz <3565087505@qq.com>
2023-04-06 07:59:02 +0800

    2023-04-06 07:57:59

---
+++ String和Integer/src/com/wzres/StrOBj2.java
@@ -0,0 +1,19 @@
+package com.wzres;
+
+/**
+ * @ClassName：StrOBj2
+ * @description：
+ * @date：2023-04-06 07:45
+ */
+public class StrOBj2 {
+    public static void main(String[] args) {
+         byte many [] = {97,98,99};
+         String strOBj = new String(many);
+        System.out.println(strOBj);
+
+        char chas [] = {97,98,99};
+        String strObj = new String(chas);
+        System.out.println(strOBj);
+
+    }
+}
