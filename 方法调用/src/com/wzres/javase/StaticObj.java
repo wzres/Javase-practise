710d06f94dbb557035ceb60c8c33f8ff3378188f
wz <3565087505@qq.com>
2023-04-06 09:14:08 +0800

    方法调用：代码之道

---
+++ 方法调用/src/com/wzres/javase/StaticObj.java
@@ -0,0 +1,47 @@
+package com.wzres.javase;
+
+/**
+ * @ClassName：StaticObj
+ * @description：
+ * @date：2023-04-06 07:45
+ */
+public class StaticObj {
+    private static int num1;
+    private static int num2;
+
+    public static void main(String[] args) {
+         byte many [] = {97,98,99};
+         String strOBj = new String(many);
+        System.out.println(strOBj);
+
+        char chas [] = {97,98,99};
+        String strObj = new String(chas);
+        System.out.println(strOBj);
+
+        // 这种方法创建比较常用，推荐
+        String s1 = "123";
+        String s2 = "123";
+
+        num1 = 12;
+        num2 = 12;
+
+        System.out.println(transfer(num1, num2));
+        System.out.println(test());
+
+        StaticObj strOBj2 = new StaticObj();
+        strOBj2.example();
+
+    }
+
+    public static int  transfer(int a,int b){
+            return a + b; //24;
+    }
+
+    public static int test(){
+        return num1-num2;
+    }
+
+    public void example(){
+        System.out.println(num1+num2); //24
+    }
+}
